b2e97485c1c59974934d9b69947f5dfb6d4a06fc
skrishnan1786 <skrishnan@dunnsolutions.com>
2019-11-12 14:48:37 -0600

    Enhancements to existing library by felipechang@hardcake.org

---
+++ app/search/Basic/customerSearchBasic.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const Basic = require("./common/Basic");
+
+class CustomerSearchBasic extends Basic {
+
+    constructor() {
+        super();
+        this._type = "platformCommon";
+        this._name = "CustomerSearchBasic";
+    }
+}
+
+module.exports = CustomerSearchBasic;
